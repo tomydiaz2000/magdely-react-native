4d91e148b71051cbb93fd1f1a17350333b20491e
Tomas Diaz <tomas.diaz@cintelink.com.ar>
2021-09-06 23:41:20 -0300

    dependencias

--- navigation/BreadNavigator.js
+++ navigation/BreadNavigator.js
@@ -1,16 +1,29 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
 
 import OrdersScreen from '../screens/OrdersScreen';
+import OrderDetailScreen from '../screens/OrderDetailScreen';
 import ProductsScreen from '../screens/ProductsScreen';
+
+const OrderStack = createNativeStackNavigator();
+
+function OrderStackScreen() {
+  return (
+    <OrderStack.Navigator>
+      <OrderStack.Screen name="Orders" component={OrdersScreen} />
+      <OrderStack.Screen name="Details" component={OrderDetailScreen} />
+    </OrderStack.Navigator>
+  );
+}
 
 const Tab = createBottomTabNavigator();
 
 const BreadNavigator = () => (
   <NavigationContainer>
     <Tab.Navigator>
-      <Tab.Screen name="Orders" component={OrdersScreen} />
+      <Tab.Screen name="Orders" component={OrderStackScreen} />
       <Tab.Screen name="Products" component={ProductsScreen} />
     </Tab.Navigator>
   </NavigationContainer>
