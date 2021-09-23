5fcfe793867140782e0d223b8d00e62beb2c20a7
Tomas Diaz <tomas.diaz@cintelink.com.ar>
2021-09-22 22:23:20 -0300

    ultimos cambios, vista login

--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -1,16 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React, { useCallback, useReducer } from 'react';
+import { Alert, StyleSheet } from 'react-native';
 import AuthScreenWrapper from '../../components/AuthScreenWrapper';
+import Input from '../../components/Input';
+import { useDispatch } from 'react-redux';
+import { formReducer, FORM_INPUT_UPDATE } from './formReducer';
+import { Button } from 'react-native-elements';
+import { login } from '../../store/actions/auth.action';
 
 const LoginScreen = () => {
+  const dispatch = useDispatch();
+  const [formState, formDispatch] = useReducer(formReducer, {
+    inputValues: {
+      email: '',
+      password: '',
+    },
+    inputValidities: {
+      email: false,
+      password: false,
+    },
+    formIsValid: false,
+  });
+
+  const handleLogIn = () => {
+    if (formState.formIsValid) {
+      dispatch(login(formState.inputValues.email, formState.inputValues.password));
+    } else {
+      Alert.alert(
+        'Formulario inválido',
+        'Ingresa email y usuario válido',
+        [{ text: 'Ok' }]
+      );
+    }
+  }
+  
+  const onInputChangeHandler = useCallback((inputIdentifier, inputValue, inputValidity) => {
+    formDispatch({
+      type: FORM_INPUT_UPDATE,
+      value: inputValue,
+      isValid: inputValidity,
+      input: inputIdentifier,
+    });
+  }, [formDispatch]);
+
   return (
     <AuthScreenWrapper
       title="INGRESAR"
       message="¿Aún no tienes cuenta?"
       buttonText="Ir al registro"
-      buttonPath="Register"
-    >
-      <Text>formulario</Text>
+      buttonPath="Register">
+      <Input
+        id="email"
+        label="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        errorText="Por favor ingresa un email válido"
+        required
+        email
+        onInputChange={onInputChangeHandler}
+      />
+      <Input
+        id="password"
+        label="Password"
+        secureTextEntry
+        autoCapitalize="none"
+        errorText="Ingrese contraseña"
+        required
+        onInputChange={onInputChangeHandler}
+      />
+      <Button
+        title="INGRESAR"
+        onPress={handleLogIn}
+        buttonStyle={styles.button}
+      />
     </AuthScreenWrapper>
   );
 }
